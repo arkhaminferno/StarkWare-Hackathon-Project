8104d192fc5b5143841303d3aeb0827d1d7f29e3
Kumar Gaurav <42605321+arkhaminferno@users.noreply.github.com>
2020-07-20 16:38:52 +0530

    Update Game.sol
    added functions and minor fixes

--- Game.sol
+++ Game.sol
@@ -2,22 +2,33 @@ pragma solidity ^0.5.2;
 
 
 contract Game {
+    
+//=============================================================================
+
+// Variables Declaration
+
+//=============================================================================
+
+    //owner's address
     address owner;
-    uint gameCounter; // Number of matches happened
     
-    constructor() public{
-        owner = msg.sender;
-    }
+    // rng contract's address    
+    address BeaconContract;
+    
+    // Number of Contests happened
+    uint public gameCounter;
+    
+    // User Balance Check 
+    mapping(address=>uint) private UserDetails;
+    
+    // check winner for a specific game 
+    mapping(uint=>address) public winnerForaGame; 
+    
+    // Bet Details for a specific contest
+    mapping(uint=>BetDetail[]) public BetDetails;
     
     
-    
-    modifier onlyOwner(){
-    require(msg.sender == owner);
-        _;
-    }
-    
-   
-    
+    // Bet Detail Struct
     struct BetDetail {
         address userAddress;
         uint numberChoosen;
@@ -26,27 +37,71 @@ contract Game {
     }
     
     
-     
-    mapping(address=>uint) private UserDetails;
-    
-    mapping(uint=>address) public winnerForaGame; // check who was the winner for a specific game
-    
-    mapping(uint=>BetDetail[]) public BetDetails;
-    
+//=============================================================================
+
+// Events Declaration
+
+//=============================================================================
     
     event depositMade(address indexed ,uint amount,uint choosenNumber);
+    
     event widthdrawMade(address indexed,uint amount);
+
+
+//=============================================================================
+
+// Constructor Declaration
+
+//=============================================================================
+    
+    
+    constructor() public{
+        owner = msg.sender;
+    }
+    
+
+//=============================================================================
+
+// Modifiers Declaration
+
+//=============================================================================
+    modifier onlyOwner(){
+    require(msg.sender == owner);
+        _;
+    }
+    
    
+
+//=============================================================================
+
+// Function Declaration
+
+//=============================================================================
+    
+    
+     
+   /** @dev Store some ether into the bankRollBalance
+    * @param amount of ether
+    */ 
    
    function depositInBankRoll() public payable onlyOwner {
        require(msg.value != 0 ether);
    }
    
+    
+   /** @dev Check Bankroll Balance
+    */ 
    
    function bankRollBalance() public view onlyOwner returns(uint) {
        return address(this).balance;
    }
    
+    
+   /** @dev Checking whether user already exists on the game or not
+    * @param contest Number
+    * @param address of the user/better
+    */ 
+    
    function checkBet(uint contestNumber,address better) internal returns(bool success){
        for(uint i=0;i<BetDetails[contestNumber].length;i++){
            if(BetDetails[contestNumber][i].userAddress == better){
@@ -60,6 +115,28 @@ contract Game {
    }
    
    
+     /** @dev Set Beacon Contract's Address
+    * @param Beacon Contract Address
+    */ 
+    
+   function setBeaconContractAddress(address _address) public onlyOwner {
+       BeaconContract = _address;
+   }
+   
+   
+     /** @dev Generate Random Number using Beacon Contract
+    */ 
+    
+   function generateRandomNumber() internal returns (uint) {
+       
+       
+   }
+   
+   
+     /** @dev Bet Amount 
+    * @param input number between 0 to 9
+    * @param input amount of ether
+    */ 
    
    function bet(uint choosenNumber ) payable public {
        require(choosenNumber>=0 && choosenNumber<=9,"Please Choose Number Between 0 and 9!");
@@ -91,12 +168,18 @@ contract Game {
        
    }
    
+   
+   
+     /** @dev Withdraw Win Amount 
+    */ 
+   
    function withdraw() public returns (bool success){
        require(UserDetails[msg.sender] > 0 , "Whoops! You are trying to withdraw zero ETH");
        require(UserDetails[msg.sender]<address(this).balance,"Please try again, Bankroll is out of Balance");
        uint amount = UserDetails[msg.sender];
        UserDetails[msg.sender] = 0;
        msg.sender.transfer(amount);
+       emit widthdrawMade(msg.sender ,amount);
        return true;
    }
    
