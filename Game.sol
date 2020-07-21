bfdb4646bf9f650df7ac6ff59bcf17965d1b8d21
Kumar Gaurav <42605321+arkhaminferno@users.noreply.github.com>
2020-07-21 19:15:19 +0530

    Update Game.sol
    Added functions

--- Game.sol
+++ Game.sol
@@ -13,7 +13,7 @@ contract Game {
 //=============================================================================
 
     //owner's address
-    address owner;
+    address public owner;
     
     // Pausable Contract
     bool isContractPaused;
@@ -22,10 +22,14 @@ contract Game {
     address public  BeaconContractAddress = 0x79474439753C7c70011C3b00e06e559378bAD040;
     
     // Number of Contests happened
-    uint public gameCounter;
+    uint public gameCounter = 1;
+    
+    //total amount staked for a contest
+    
+    mapping(uint=>uint) public stakedAmountforContest; 
     
     // User Balance Check 
-    mapping(address=>uint) private UserDetails;
+    mapping(address=>uint) public UserDetails;
     
     // check winner for a specific game 
     mapping(uint=>address) public winnerForaGame; 
@@ -106,12 +110,12 @@ contract Game {
    /** @dev Checking whether user already exists on the game or not
     */ 
     
-   function checkBet(uint contestNumber,address better) internal view returns(bool success){
+   function checkBet(uint contestNumber,uint choosennum) internal view returns(bool success){
        require(isContractPaused == false);
        for(uint i=0;i<BetDetails[contestNumber].length;i++){
-           if(BetDetails[contestNumber][i].userAddress == better){
+           if(BetDetails[contestNumber][i].numberChoosen == choosennum){
                return false;
-               revert("you have already participated this contest");
+               revert("Someone already Choosed this number");
            }
            else{
                return true;
@@ -131,25 +135,38 @@ contract Game {
      /** @dev Generate Random Number using Beacon Contract
     */ 
     
-   function generateRandomNumber() public view returns ( bytes32) {
+   function generateRandomNumber() public view returns (uint) {
        uint blockNumber;
        bytes32 randomNumber;
        Beacon beacon = Beacon(BeaconContractAddress);
        (blockNumber,randomNumber) = beacon.getLatestRandomness();
-       return randomNumber;
+       return uint(randomNumber);
        
    }
    
+   /** Picking Winner by checking bet Details 
+    */
+    function pickWinner(uint randomNumber,uint currentGameCounter) internal  returns(address winner) {
+        for(uint i=0;i<BetDetails[currentGameCounter].length;i++){
+            if(BetDetails[currentGameCounter][i].numberChoosen == randomNumber){
+                gameCounter++;
+                return BetDetails[currentGameCounter][i].userAddress;
+            }
+            else{
+                revert("No one has bet on this number");
+            }
+        }
+    }
    
      /** @dev Bet Amount 
     */ 
    
-   function bet(uint choosenNumber ) payable public {
+   function bet(uint choosenNumber ) payable public returns (bool success){
        require(isContractPaused == false);
        require(choosenNumber>=0 && choosenNumber<=9,"Please Choose Number Between 0 and 9!");
        require(msg.value >= 0.2 ether , "Please Enter Amount Greater than 0.2 ether");
-       gameCounter++;
-       require(checkBet(gameCounter,msg.sender) == true);
+      
+       require(checkBet(gameCounter,choosenNumber) == true);
        
        BetDetail memory newBet;
        newBet.userAddress = msg.sender;
@@ -157,21 +174,33 @@ contract Game {
        newBet.betAmount = msg.value;
        
        BetDetails[gameCounter].push(newBet);
+       
        delete newBet;
        
-       /* TODO
-       * random number generation logic
-       */
+       
+       emit depositMade(msg.sender,msg.value,choosenNumber);
        
        
        /* TODO
        * if number number of bets for a match has been full generate random number else not
        */
        
-       /* TODO 
-       * if user is the winner then store the winning amount into User structs
-       */
+       if(BetDetails[gameCounter].length == 10){
+           uint generatedNumber = generateRandomNumber();
+            address winner = pickWinner(generatedNumber,gameCounter);
        
+       winnerForaGame[gameCounter] = winner;
+       uint Am = 0.5 ether;
+       UserDetails[winner] += 0.5 ether;
+       owner.transfer(0.5 ether);
+       gameCounter++;
+       return true;
+           
+       }
+       return true;
+     
+
+      
        
    }
    
